5e7150083a5f1bc6b7d042f04c11a74426b87ed1
bni <bnilsson@me.com>
2017-03-25 10:13:49 +0100

    Switch to using serverless-plugin-include-dependencies

--- binary/index.js
+++ binary/index.js
@@ -3,27 +3,17 @@
 const fs = require("fs");
 
 module.exports.fetchBinary = (event, context, callback) => {
-  try {
-    const content = fs.readFileSync("binary/sample.pdf");
+  const content = fs.readFileSync("binary/sample.pdf");
 
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/pdf"
-      },
-      body: content.toString("base64"),
-      isBase64Encoded: true
-    };
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/pdf",
+      "Content-Disposition": "inline; filename=\"sample.pdf\""
+    },
+    body: content.toString("base64"),
+    isBase64Encoded: true
+  };
 
-    return callback(null, response);
-  } catch (errorMessage) {
-    console.error(errorMessage);
-
-    const response = {
-      statusCode: 500,
-      body: errorMessage
-    };
-
-    return callback(null, response);
-  }
+  return callback(null, response);
 };
--- text/index.js
+++ text/index.js
@@ -1,24 +1,15 @@
 "use strict";
 
+const moment = require("moment");
+
 module.exports.fetchText = (event, context, callback) => {
-  try {
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "text/plain"
-      },
-      body: "Hello World!"
-    };
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/plain"
+    },
+    body: `Today is ${moment().format("dddd")}.`
+  };
 
-    return callback(null, response);
-  } catch (errorMessage) {
-    console.error(errorMessage);
-
-    const response = {
-      statusCode: 500,
-      body: errorMessage
-    };
-
-    return callback(null, response);
-  }
+  return callback(null, response);
 };
