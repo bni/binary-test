1d79c78a3e2dc57694a64eefea459a8f5c1035d5
bni <bnilsson@me.com>
2017-03-27 22:21:04 +0200

    Experiments...

--- text/index.js
+++ text/index.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const moment = require("moment");
-const S3 = require("aws-sdk/clients/s3");
-//const request = require("request");
+const S3 = require("aws-sdk/clients/s3"); // Not used, for now
+const request = require("request"); // Not used, for now
+
+// Hack around broken soap module that tries to read .ejs files that dont exist
+const mock = require("mock-require"); mock("fs", {readFileSync: () => {return "";}});
+const soap = require("soap"); mock.stop("fs");
 
 module.exports.fetchText = (event, context, callback) => {
   const response = {
